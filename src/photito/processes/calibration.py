c87cd17ce25e5b8246f2b7cb77b644fbfd7c50f7
Ben Godson <ben_godson@ymail.com>
2025-01-30 10:55:00 +0000

    Add combine function for darks

--- src/photito/processes/calibration.py
+++ src/photito/processes/calibration.py
@@ -1,6 +1,6 @@
 from astropy.io import fits
 import numpy as np
-
+import logging
 import ccdproc as ccdp
 from ..image_sets import BiasSet
 
@@ -14,7 +14,7 @@ def combine_bias(files: list, output: str):
     hdu = fits.PrimaryHDU(bias)
     hdu.writeto(output, overwrite=True)
 
-def combine_bias_ccdproc(files:list, output:str):
+def combine_bias_ccdproc(files: list, output: str):
     """Combine bias frames using ccdproc."""
     # Read bias frames
     bias_frames = [ccdp.CCDData.read(file, unit='adu') for file in files]
@@ -22,3 +22,19 @@ def combine_bias_ccdproc(files:list, output:str):
     bias = ccdp.combine(bias_frames, method='median')
     # Save combined bias
     bias.write(output, overwrite=True)
+
+def combine_darks_ccdproc(files: list, output: str, validate=True, mem_limit=32e9):
+    """Combine dark frames using ccdproc."""
+    for file in files:
+        image_type = fits.getval(file, 'IMAGETYP', ext=0).lower()
+        if image_type != 'dark':
+            if validate:
+                raise ValueError(f'Image {file} is not a dark frame.')
+            else:
+                logging.warning(f'Image {file} is not a dark frame.')
+    # Combine dark frames
+    dark = ccdp.combine(files, method='average', unit='adu',
+                        sigma_clip=True, mem_limit=mem_limit, dtype=np.float32)
+    # Save combined dark
+    dark.meta['combined'] = True
+    dark.write(output, overwrite=True)
