feedf08dbeff8319be52060276a90c8e24f18470
Ben Godson <ben_godson@ymail.com>
2025-02-06 13:41:55 +0000

    Base logger

--- src/photito/__init__.py
+++ src/photito/__init__.py
@@ -1,5 +1,10 @@
 from importlib.metadata import version as _version, PackageNotFoundError
+import logging
+import sys
+
 try:
     __version__ = _version(__name__)
 except PackageNotFoundError:
     pass
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
