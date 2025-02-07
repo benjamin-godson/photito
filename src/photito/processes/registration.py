e21ffc54ff38fbf7b20243a6b76673ecc55c5cb5
Ben Godson <ben_godson@ymail.com>
2025-02-07 12:24:24 +0000

    Change default sep threshold

--- src/photito/processes/registration.py
+++ src/photito/processes/registration.py
@@ -10,12 +10,13 @@ from shutil import copyfile
 
 logger = logging.getLogger(__name__)
 
-def align_lights(files: list, output_dir: str, reference: str = None, n_processes: int = 1):
+def align_lights(files: list, output_dir: str, reference: str = None, n_processes: int = 1, threshold: float = 5):
     """Align light frames to a reference frame.
     :param files: List of light frames.
     :param output_dir: Output folder.
     :param reference: Reference frame.
     :param n_processes: Number of processes to use.
+    :param threshold: Detection threshold for source extraction.
     """
     logger.info(f'Aligning {len(files)} light frames.')
     if reference is None:
@@ -27,11 +28,11 @@ def align_lights(files: list, output_dir: str, reference: str = None, n_processe
     reference_data = fits.getdata(reference)
     reference_data = np.nan_to_num(reference_data)
     logger.info('Extracting sources from reference frame.')
-    reference_detections = reference_extraction(reference_data)
+    reference_detections = reference_extraction(reference_data, threshold)
     logger.info(f"Found {len(reference_detections)} sources in reference frame.")
     if n_processes > 1:
         with Pool(n_processes) as pool:
-            pool.starmap(align_light_thread, [(file, reference_detections, output_dir) for file in files])
+            pool.starmap(align_light_thread, [(file, reference_detections, output_dir, threshold) for file in files])
     else:
         for file in files:
             align_light_thread(file, reference_data, output_dir)
@@ -53,7 +54,7 @@ def align_light_thread(file: str, reference_detections: Table, output_dir: str):
     combined_data = CCDData(data=spalipy.aligned_data, mask=spalipy.aligned_mask, header=image_header, unit='adu')
     combined_data.write(f'{output_dir}/aligned_{file.split("/")[-1]}', overwrite=True)
 
-def reference_extraction(data: np.ndarray, threshold: float = 1.5):
+def reference_extraction(data: np.ndarray, threshold: float = 5):
     """Extract sources from a reference frame."""
     if data.dtype.byteorder == '>':
         data = data.byteswap().newbyteorder()
