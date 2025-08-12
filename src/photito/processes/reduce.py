6b2d2b35a4a53f522c9cf612b3d8ed040a44cd9d
Ben Godson <ben_godson@ymail.com>
2025-08-12 11:28:01 +0100

    ccdproc combination for lights

--- src/photito/processes/reduce.py
+++ src/photito/processes/reduce.py
@@ -1,9 +1,13 @@
 import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 from photito import config
-
+import ccdproc as ccdp
 from astropy.stats import mad_std, sigma_clip
+from astropy.io import fits
 from functools import partial
+from itertools import repeat
+import logging
+
 
 class Stacker:
     def __init__(self,
@@ -47,24 +51,25 @@ class Stacker:
             self.weights = np.ones_like(self.data, dtype=float)
 
         n_sections = min(self.n_sections, self.data.shape[1])
+        logging.info(f'Stacking data in {n_sections} sections.')
 
         sections = np.array_split(self.data, n_sections, axis=1)
         if self.mask is not None:
             mask_sections = np.array_split(self.mask, n_sections, axis=1)
         else:
-            mask_sections = [None] * n_sections
+            mask_sections = repeat(None)
         if self.uncertainty is not None:
             uncertainty_sections = np.array_split(self.uncertainty, n_sections, axis=1)
         else:
-            uncertainty_sections = [None] * n_sections
+            uncertainty_sections = repeat(None)
         if self.scaling is not None:
             scaling_sections = np.array_split(self.scaling, n_sections, axis=1)
         else:
-            scaling_sections = [None] * n_sections
+            scaling_sections = repeat(None)
         if self.weights is not None:
             weights_sections = np.array_split(self.weights, n_sections, axis=1)
         else:
-            weights_sections = [None] * n_sections
+            weights_sections = repeat(None)
 
         with ThreadPoolExecutor(max_workers=config.n_processes) as executor:
             results = list(executor.map(self._average_sigma_clip,
@@ -76,9 +81,6 @@ class Stacker:
                                         [sigma] * n_sections,
                                         [maxiters] * n_sections))
             return np.concatenate(results, axis=1).astype(np.float32)
-
-
-
 
     def _average_sigma_clip(self,
                             data: np.ndarray = None,
@@ -97,12 +99,39 @@ class Stacker:
                                         sigma=sigma,
                                         maxiters=maxiters,
                                         stdfunc=mad_std,
-                                        cenfunc=np.ma.median,)
+                                        cenfunc=np.ma.median)
 
         stacked_data = np.ma.average(sigma_clipped_data, axis=0, weights=weights)
         return stacked_data.filled(fill_value=np.nan)
 
 
-
-
-
+def combine_lights_ccdproc(files: list, output: str, mem_limit=32e9,
+                           sigma_clip: bool = True,
+                           sigma_clip_low_thresh=3,
+                           sigma_clip_high_thresh=3,
+                           combine_method='average',
+                           dtype=np.float32):
+    """Combine light frames.
+    :param files: List of light frames.
+    :param output: Output file name.
+    :param mem_limit: Memory limit for the operation.
+    :param sigma_clip: Use sigma clipping.
+    :param sigma_clip_low_thresh: Low threshold for sigma clipping.
+    :param sigma_clip_high_thresh: High threshold for sigma clipping.
+    :param combine_method: Method for combining the frames.
+    :param dtype: Data type for the output.
+    """
+    # Combine light frames
+    master = ccdp.combine(files, method=combine_method, unit='adu',
+                          sigma_clip=sigma_clip, sigma_clip_low_thresh=sigma_clip_low_thresh,
+                          sigma_clip_high_thresh=sigma_clip_high_thresh,
+                          mem_limit=mem_limit, dtype=dtype)
+    # Save combined light
+    master.meta['combined'] = True
+    master.meta['n_combine'] = len(files)
+    master.meta['combine_method'] = combine_method
+    master.meta['sigma_clip'] = sigma_clip
+    if sigma_clip:
+        master.meta['sigma_clip_low_thresh'] = sigma_clip_low_thresh
+        master.meta['sigma_clip_high_thresh'] = sigma_clip_high_thresh
+    master.write(output, overwrite=True)
